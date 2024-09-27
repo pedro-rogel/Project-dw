3ffa696626be3e7b19b37d904b1318938de925cc
Pedro Rogel <pedrorogel3@gmail.com>
2024-09-27 16:54:39 -0300

    validando o login

--- project-dw/script/login.js
+++ project-dw/script/login.js
@@ -1,7 +1,7 @@
 const url = "https://go-wash-api.onrender.com/api/login";
 const login = async () => {
-  let email = document.getElementsByClassName("emai");
-  let senha = document.getElementsByClassName("senha");
+  let email = document.getElementById("emai");
+  let senha = document.getElementById("senha");
 
   if (!email) {
     alert("Passe um valor para o campo email");
@@ -13,25 +13,27 @@ const login = async () => {
     return;
   }
 
-  let api = fetch(url, {
+  let api = await fetch(url, {
     method: "POST",
     body: JSON.stringify({
       name: "xxxxxxxxxxx",
-      email: "xxxxxxxxx@gmail.com",
+      email: email,
       user_type_id: 1,
-      password: "123456",
+      password: senha, 
       cpf_cnpj: "62418247406",
       terms: 1,
       birthday: "2000-10-12",
     }),
     headers: {
-      "Content-Type": "aplication/json",
+      'Content-Type': 'application/json',
+      'Cookie': 'gowash_session=0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj'
     },
   });
-
-  let resp = await api.json() 
+  
+  let resp = await api.json();
+  
 
   if(pass)
-    pass
+    passd
 
 };
