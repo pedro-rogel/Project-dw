c2148daa1b9957534e6f3ff7cff164b95b2c4e03
Pedro Rogel <pedrorogel3@gmail.com>
2024-09-30 15:09:43 -0300

    Criando o login (marcação e estilização)

--- project-dw/script/login.js
+++ project-dw/script/login.js
@@ -1,8 +1,10 @@
 const url = "https://go-wash-api.onrender.com/api/login";
+const botao = document.getElementById("botao-input");
 const login = async () => {
-  let email = document.getElementById("emai");
-  let senha = document.getElementById("senha");
-
+  let email = document.getElementById("email").value;
+  let senha = document.getElementById("password").value;
+  let submit = document.getElementById("botao-input")
+  
   if (!email) {
     alert("Passe um valor para o campo email");
     return;
@@ -16,24 +18,22 @@ const login = async () => {
   let api = await fetch(url, {
     method: "POST",
     body: JSON.stringify({
-      name: "xxxxxxxxxxx",
-      email: email,
+      email: email, 
       user_type_id: 1,
       password: senha, 
-      cpf_cnpj: "62418247406",
-      terms: 1,
-      birthday: "2000-10-12",
     }),
     headers: {
-      'Content-Type': 'application/json',
-      'Cookie': 'gowash_session=0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj'
+      "Content-Type": "application/json",
+      Cookie: "gowash_session=0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj",
     },
   });
-  
+
   let resp = await api.json();
   
-
-  if(pass)
-    passd
+  if (resp.data.errors){
+    alert(resp.data.errors)
+  }
 
 };
+
+botao.addEventListener("click", login)
