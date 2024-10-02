3d93ad1c6cfdb63281d160d38933fdf3f38e26ab
Pedro Rogel <pedrorogel3@gmail.com>
2024-10-02 16:06:42 -0300

    Criando o js do login

--- project-dw/script/login.js
+++ project-dw/script/login.js
@@ -3,8 +3,7 @@ const botao = document.getElementById("botao-input");
 const login = async () => {
   let email = document.getElementById("email").value;
   let senha = document.getElementById("password").value;
-  let submit = document.getElementById("botao-input");
-  
+
   if (!email) {
     alert("Passe um valor para o campo email");
     return;
@@ -18,35 +17,30 @@ const login = async () => {
   let api = await fetch(url, {
     method: "POST",
     body: JSON.stringify({
-      email: email, 
+      email: email,
       user_type_id: 1,
-      password: senha, 
+      password: senha,
     }),
     headers: {
       "Content-Type": "application/json",
-      Cookie: "gowash_session=0hGqRHf0q38ETNgEcJGce30LcPtuPKo48uKtb7Oj",
     },
   });
 
-  let resp = await api.json();
-  console.log(resp);
-  //localStorage.setItem("user", JSON.stringify(resp));
-
-
-  if (resp.data.errors){
-    alert(resp.data.errors);
-    alert('Deu erro');
-  } else {
-    alert('Deu certo');
+  if (api.ok) {
+    let resp = await api.json();
+    console.log(resp);
+    localStorage.setItem("user", JSON.stringify(resp));
+    alert('Login bem sucedido');
     window.location.href = "../view/home.html";
+  } else {
+    let respErro = await api.json();
+    alert(respErro.data.errors);
   }
-
 };
 
 const cadastroEndereco = () => {
-  //let user = JSON.parse(localStorage.getItem("user"))
+  let user = JSON.parse(localStorage.getItem("user"));
   console.log(user.access_token);
-}
+};
 
 botao.addEventListener("click", login);
-
