7b3d9e043e8dc5ca6021e227114ce0a7920cbb8c
Pedro Rogel <pedrorogel3@gmail.com>
2024-10-30 14:12:52 -0300

    Add a function delet address

--- project-dw/script/home.js
+++ project-dw/script/home.js
@@ -1,3 +1,8 @@
+const urlBase = "https://go-wash-api.onrender.com/api/auth/address/";
+const params = new URLSearchParams(window.location.search);
+const addressId = params.get("id");
+const url = `${urlBase}${addressId}`;
+
 const addEndereco = document.querySelector(".svg");
 addEndereco.addEventListener("click", () => {
   window.open("../view/endereco.html", "_blank");
@@ -11,23 +16,24 @@ const renderizarEnderecos = (end) => {
   end.forEach((endereco) => {
     const li = document.createElement("li");
     li.classList.add("cartao-endereco");
-    li.innerHTML = ` <div class="informacoes">
+    li.innerHTML = `
+      <div class="informacoes">
         <span>${endereco.title}</span>
         <span>${endereco.number}</span>
       </div>
       <img src="../img/img-fundo-index.jpg" alt="${endereco.title}" class="img" />
-      <ul class="edicao">
-        <li class="excluir-end">
+      <ul class="edicao" id="${endereco.id}">
+        <li class="excluir-end" data-id="${endereco.id}">
           Excluir
-          <a href="">
+          <a href="#">
             <img src="../img/trash.svg" alt="Lixeira Excluir" />
           </a>
         </li>
-        <li class="editar-end" id="${endereco.id}">
-        <a href="../view/update-address.html?id=${endereco.id}
-        ">
-          Editar 
-          <img src="../img/pencil.svg" alt="login Edição" /></a>
+        <li class="editar-end">
+          <a href="../view/update-address.html?id=${endereco.id}">
+            Editar 
+            <img src="../img/pencil.svg" alt="login Edição" />
+          </a>
         </li>
       </ul>
       <p class="descricao">${endereco.address} - ${endereco.cep}</p>
@@ -37,30 +43,52 @@ const renderizarEnderecos = (end) => {
 
   const liAdd = document.createElement("li");
   liAdd.classList.add("cartao-endereco_add");
-  liAdd.innerHTML = ` <div class="informacoes_add">
+  liAdd.innerHTML = ` 
+    <div class="informacoes_add">
       <span>Adicionar Endereço</span>
     </div>
-
-
-  <a  href="endereco.html">
-      <svg
-      class="svg"
-      xmlns="http://www.w3.org/2000/svg"
-      width="16"
-      height="16"
-      fill="currentColor"
-      class="bi bi-plus"
-      viewBox="0 0 16 16"
-    >
-      <path
-        d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"
-      />
-    </svg>`;
+    <a href="endereco.html">
+      <svg class="svg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+      </svg>
+    </a>
+  `;
 
   listaEndereco.appendChild(liAdd);
+
+  
+  const output = document.querySelector(".output");
+
+  document.querySelectorAll(".excluir-end").forEach((botao) => {
+    botao.addEventListener("click", async () => {
+      const addressId = botao.getAttribute("data-id");
+
+      output.classList.toggle("ativo")
+
+      output.innerHTML = `<div class="card">
+      <p>Deseja mesmo excluir esse endereço?</p>
+      <div class="opcoes">
+      <button id="sim">Sim</button>
+      <button id="nao">Não</button>
+      </div>
+      </div>`;
+
+      document.getElementById("sim").addEventListener("click", async () => {
+        
+        await deleteAddress(addressId);
+        output.classList.toggle("ativo");
+        output.innerHTML = "";
+        
+      });
+
+      document.getElementById("nao").addEventListener("click", () => {
+        output.innerHTML = "";
+        output.classList.toggle("ativo")
+      });
+    }); 
+  });
 };
 
-const urlGet = "https://go-wash-api.onrender.com/api/auth/address";
 const getEndereco = async () => {
   let user = JSON.parse(localStorage.getItem("user"));
   if (!user || !user.access_token) {
@@ -68,7 +96,7 @@ const getEndereco = async () => {
     return;
   }
 
-  let api = await fetch(urlGet, {
+  let api = await fetch(urlBase, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -81,8 +109,27 @@ const getEndereco = async () => {
     console.log(resp);
     renderizarEnderecos(resp.data);
   } else {
-    alert("erro");
+    alert("Erro ao carregar endereços.");
   }
 };
 
+
+
+const deleteAddress = async (addressId) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const response = await fetch(`${urlBase}${addressId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${user.access_token}`,
+    },
+  });
+
+  if (response.ok) {
+    alert("Endereço excluído com sucesso!");
+    getEndereco();    
+  } else {
+    alert("Erro ao excluir endereço!");
+  }
+};
 document.addEventListener("DOMContentLoaded", getEndereco);
