6f303288478a01644f4f25700f223a102fc1f589
Pedro Rogel <pedrorogel3@gmail.com>
2024-11-12 13:05:11 -0300

    Mudando o window.location para o index

--- project-dw/script/logout.js
+++ project-dw/script/logout.js
@@ -2,9 +2,10 @@ const urlapi = 'https://go-wash-api.onrender.com/api/auth/logout'
 const user = JSON.parse(localStorage.getItem("user"));
 const logout = async () => {
     if (!user || !user.access_token) {
-        alert("Usuário não está autenticado. Faça o login novamente.");
-        window.location.href = '/project-dw/view/login.html';
-      }
+        alert("Usuário não está autenticado.");
+        window.location.href = '/project-dw/index.html';
+        return;
+      };
     let apiLogout = await fetch(urlapi,{
         method: 'POST',
         headers: {
@@ -16,8 +17,8 @@ const logout = async () => {
     if (apiLogout.ok) {
         let resp = await apiLogout.json();
         console.log(resp);
-        alert(resp.data)
-        localStorage.removeItem('user')
+        alert(resp.data);
+        localStorage.removeItem('user');
         window.location.href = '../index.html'
         return;
 
